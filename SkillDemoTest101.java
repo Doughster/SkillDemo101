7791517bd028fe54ea6f74fa6f92119a9dce872c
Doughster <jtdo@ucsd.edu>
2022-02-04 16:31:32 -0800

    Created libs and tests for SkillDemo

---
+++ SkillDemoTest101.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+ 
+public class SkillDemoTest101 {
+    @Test
+    public void toMultiply() {
+        assertEquals(10, SkillDemo101.multiply(3,5));
+    }
+}
